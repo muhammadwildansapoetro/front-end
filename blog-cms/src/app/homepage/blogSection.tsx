7aeba36cab1b04d726acaeda5a3761ca932a1781
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-11-07 16:06:34 +0700

    update layout

--- blog-cms/src/app/homepage/blogSection.tsx
+++ blog-cms/src/app/homepage/blogSection.tsx
@@ -11,7 +11,7 @@ export default async function BlogSection() {
         <h1 className="text-center font-bold text-3xl lg:text-6xl mb-5">
           Our Articles
         </h1>
-        <div className="flex flex-col items-center lg:flex-row lg:justify-evenly ">
+        <div className="flex lg:flex-wrap lg:flex-row flex-col items-center lg:justify-start ">
           {data.map((item, index) => {
             return (
               <div
