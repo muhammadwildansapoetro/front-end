28d112fca95d5f35cff6172a38be3cc5f645faae
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-11-13 14:23:32 +0700

    remove unused font

--- company-profile/src/app/layout.tsx
+++ company-profile/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/header/navBar";
 import Footer from "@/components/footer";
